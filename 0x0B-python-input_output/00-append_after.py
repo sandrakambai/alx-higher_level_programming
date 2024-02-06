dda9d9354e4cf264fc0131bbd299a380fe9cd6ad
Sandra Kambai <sandrachagusia@gmail.com>
2024-02-06 12:01:08 -0800

    Python input output

---
+++ 0x0B-python-input_output/00-append_after.py
@@ -0,0 +1,13 @@
+
+t@5c5e6f567046:/alx-higher_level_programming/0x0B-python-input_output# vim 100-main.py
+root@5c5e6f567046:/alx-higher_level_programming/0x0B-python-input_output# vim 101-generator.py
+root@5c5e6f567046:/alx-higher_level_programming/0x0B-python-input_output# vim 101-stats.py
+root@5c5e6f567046:/alx-higher_level_programming/0x0B-python-input_output# vim #!/usr/bin/python3
+
+[1]+  Stopped                 vim
+root@5c5e6f567046:/alx-higher_level_programming/0x0B-python-input_output# vim 11-main.py
+root@5c5e6f567046:/alx-higher_level_programming/0x0B-python-input_output# vim 11-student.py
+root@5c5e6f567046:/alx-higher_level_programming/0x0B-python-input_output# vim 12-main.py
+root@5c5e6f567046:/alx-higher_level_programming/0x0B-python-input_output# vim 12-pascal_triangle.py
+root@5c5e6f567046:/alx-higher_level_programming/0x0B-python-input_output# vim 2-append_write.py
+root@5c5e6f567046:/alx-higher_level_programmi
