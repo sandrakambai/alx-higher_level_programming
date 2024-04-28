2f39b80d5ab7cff44cedc0c56904a8d9572591c8
Sandra Kambai <sandrachagusia@gmail.com>
2024-04-28 11:12:35 +0000

    Python network

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Bash script that sends a JSON POST request to a URL passed
+curl -s -X POST -H "Content-Type: application/json" -d "@${2}" "${1}"
---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Bash script that makes a request to 0.0.0.0:5000/catch_me
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:HolbertonSchool"
